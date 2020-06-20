bbd777d6af4665d505e58197304fa07a82cd9c1e
hanifann <hanifannurulhaq@outlook.com>
2020-06-20 13:41:08 +0700

    add splash screen

--- lib/ui/loading.dart
+++ lib/ui/loading.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:copid_tracker/services/copid_api.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_splash_screen/flutter_splash_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 
 class Loading extends StatefulWidget {
@@ -8,6 +12,13 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  startTime() async{
+    var _duration = new Duration(seconds: 3);
+    return new Timer(_duration,(){
+      getCopid();
+    }
+    );
+  }
 
   void getCopid() async{
     CopidApi copidApi = new CopidApi();
@@ -22,12 +33,28 @@ class _LoadingState extends State<Loading> {
   @override
   void initState() {
     super.initState();
-    getCopid();
+    startTime();
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Hoho meme'),
+      backgroundColor: Color.fromRGBO(195, 244, 227, 1),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('assets/virus.png', scale: 2,),
+            Text('Corona Virus Tracker',
+            style: GoogleFonts.ruda(
+              textStyle: TextStyle(
+              color: Color.fromRGBO(49, 196, 146, 1),
+              fontSize: 30,
+              fontWeight: FontWeight.bold
+            ),
+            ))
+          ],
+        ),
+      )
       
     );
   }
